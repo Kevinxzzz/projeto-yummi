25e2258edea6f6dd137c1072c5691976038a16f4
kevinxzzz <kevinmaravilhason@gmail.com>
2025-11-14 23:50:44 -0300

    feat: back with prisma and new frontend

---
+++ back-end/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import usersRouter from "./src/routes/users";
+import ingredientRouter from "./src/routes/ingredients";
+import PreparationMethod from "./src/routes/preparationMethod";
+import recipeRouter from "./src/routes/recipe";
+import reviewRouter from "./src/routes/review";
+import cors from "cors";
+const app = express();
+app.use(cors());
+const port = 3000;
+
+app.use(express.json());
+
+app.use("/users", usersRouter);
+app.use("/ingredients", ingredientRouter);
+app.use("/preparationMethods", PreparationMethod);
+app.use("/recipes", recipeRouter);
+app.use("/reviews", reviewRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
---
+++ back-end/src/routes/users.ts
@@ -0,0 +1,90 @@
+import express from "express";
+import { prisma } from "../db";
+const router = express.Router();
+
+router.get("/", async (req: express.Request, res: express.Response) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).send("Error search users");
+  }
+});
+
+router.get("/:id", async (req: express.Request, res: express.Response) => {
+  try {
+    const userId = parseInt(req.params.id);
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send("Error search for this user");
+  }
+});
+
+router.get(
+  "/userName/:userName",
+  async (req: express.Request, res: express.Response) => {
+    try {
+      const userName = String(req.params.userName);
+      const user = await prisma.user.findUnique({
+        where: {
+          userName: userName,
+        },
+      });
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).send("Error search for this user");
+    }
+  }
+);
+
+router.post("/", async (req: express.Request, res: express.Response) => {
+  try {
+    const { name, userName, email, password } = req.body;
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        userName,
+        email,
+        password,
+      },
+    });
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).send({ message: "Error creating user" });
+  }
+});
+
+router.put("/:id", async (req: express.Request, res: express.Response) => {
+  try {
+    const userId = parseInt(req.params.id);
+    const { name, userName, email, password } =req.body;
+    const updateUser = await prisma.user.update({
+      where: { id: userId },
+      data: {name, userName, email, password },
+    });
+    res.status(200).json({ message: "User updated successfully", user: updateUser
+    })
+  }catch (error) {
+    res.status(500).send("Error updating user");
+  }
+})
+
+router.delete("/:id", async (req: express.Request, res: express.Response) => {
+  try {
+    const userId = parseInt(req.params.id);
+    await prisma.user.delete({
+      where: {id: userId}
+    });
+    res.status(204).send("User deleted");
+  }catch (error) {
+    res.status(500).send("Error deleting user");
+  }
+})
+
+
+export default router;
