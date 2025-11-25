f4b40020da9aebb4d5ed850a6b4a2459ef6111df
kevinxzzz <kevinmaravilhason@gmail.com>
2025-11-25 20:27:21 -0300

    feat: pagina de login integrada

--- back-end/src/routes/users.ts
+++ back-end/src/routes/users.ts
@@ -45,6 +45,18 @@ router.get(
 router.post("/", async (req: express.Request, res: express.Response) => {
   try {
     const { name, userName, email, password } = req.body;
+
+    const verifyExisting = await prisma.user.findFirst({
+      where: {
+        OR: [{ email: email }, { userName: userName }],
+      },
+    });
+    if (verifyExisting) {
+      return res
+        .status(400)
+        .json({ message: "User with this email or username already exists" });
+    }
+
     const newUser = await prisma.user.create({
       data: {
         name,
@@ -59,32 +71,55 @@ router.post("/", async (req: express.Request, res: express.Response) => {
   }
 });
 
+router.post("/login", async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password } = req.body;
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (password !== user?.password) {
+      return res.status(401).json({ message: "invalid credentials" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send("Error search for this user");
+  }
+});
+
 router.put("/:id", async (req: express.Request, res: express.Response) => {
   try {
     const userId = parseInt(req.params.id);
-    const { name, userName, email, password } =req.body;
+    const { name, userName, email, password } = req.body;
     const updateUser = await prisma.user.update({
       where: { id: userId },
-      data: {name, userName, email, password },
+      data: { name, userName, email, password },
     });
-    res.status(200).json({ message: "User updated successfully", user: updateUser
-    })
-  }catch (error) {
+    res
+      .status(200)
+      .json({ message: "User updated successfully", user: updateUser });
+  } catch (error) {
     res.status(500).send("Error updating user");
   }
-})
+});
 
 router.delete("/:id", async (req: express.Request, res: express.Response) => {
   try {
     const userId = parseInt(req.params.id);
     await prisma.user.delete({
-      where: {id: userId}
+      where: { id: userId },
     });
     res.status(204).send("User deleted");
-  }catch (error) {
+  } catch (error) {
     res.status(500).send("Error deleting user");
   }
-})
-
+});
 
 export default router;
